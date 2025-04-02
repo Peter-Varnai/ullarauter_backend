cc1ad390391213a61ca388d4a0821b6ef8b2eb5e
Peter Varnai <peter@varnai.dev>
2025-04-02 10:01:58 +0000

    in between state of the backend

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod errors;
 use actix_files::Files;
 use actix_identity::IdentityMiddleware;
 use actix_session::{SessionMiddleware, storage::CookieSessionStore};
-use actix_web::{web::Data, rt::spawn, cookie::Key, App, HttpServer};
+use actix_web::{web::Data, rt::spawn, cookie::Key, App, HttpServer, middleware::DefaultHeaders};
 use tokio::time::{Duration, sleep};
 use sqlx::sqlite::SqlitePoolOptions;
 use models::AppState;
@@ -23,23 +23,26 @@ use std::{env, path::PathBuf};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
+    let db_addr = env::current_dir().unwrap().join("db/ulla_db.db");
+    let db_url = format!("sqlite://{}", db_addr.display());
     let pool = SqlitePoolOptions::new()
-        .connect("sqlite://db/ulla_db.db")
+        .connect(&db_url)
+        // .connect("sqlite://db/ulla_db.db")
         .await
         .expect("Error building a connection pool");
-
+    
+    println!("connected to db at: {}", db_url);
     //configure the load_enf_file function to only run in
     //development mode
     load_env_file(".env");
-    // get_current_path();
 
     let secret_key = Key::generate(); // Secure key for signing cookies
     let host = env::var("HOST").unwrap_or("127.0.0.1".to_string());
-    let port = env::var("PORT").unwrap_or("8080".to_string());
+    let mut port = env::var("PORT").unwrap_or("8080".to_string());
     let password = env::var("ADMIN_PASSWORD").expect("failed to fetch admin password from enviroment variable.");
 
     // determine static file path
-    let env_static_path = env::var("STATIC_PATH").expect("failed to fetch enviroment variable");
+    let mut env_static_path = env::var("STATIC_PATH").expect("failed to fetch enviroment variable");
     let current_path = env::current_exe().unwrap();
 
     let static_path = if current_path.parent().unwrap().ends_with("debug") {
@@ -49,6 +52,11 @@ async fn main() -> std::io::Result<()> {
        PathBuf::from(&env_static_path)
     };
 
+    //reassign host, static if run in debug mode
+    if env::current_exe().unwrap().ends_with("debug"){
+        println!("running in developer mode");
+        port = "4002".to_string();
+    }
 
     let pool_clone = pool.clone(); 
 
@@ -69,6 +77,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
+            .wrap(DefaultHeaders::new().add(("X-Forwarded-Proto", "https")))
             .wrap(SessionMiddleware::new(
                     CookieSessionStore::default(),
                     secret_key.clone()))
